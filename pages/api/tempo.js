84ebe20e7c60870e47d67fc823b68421439d1a93
ArthurBuenoStein <arthurbuenostein2@gmail.com>
2021-09-23 00:35:39 -0300

    API_Funcionando

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,13 +1,15 @@
 async function tempo(request, response) {
     const dynamicDate = new Date();
 
-    const temperaturaAtual = await fetch("https://api.hgbrasil.com/weather?key=53fc40c6&city_name=Portão,RS");
+    const temperaturaAtual = await fetch("https://api.hgbrasil.com/weather?key=53fc40c6&city_name=Port%C3%A3o,RS");
     const temperaturaAtualJson = await temperaturaAtual.json();
     const temperatura = temperaturaAtualJson.results.temp;
+    const local = temperaturaAtualJson.results.city;
 
     response.json({
         date: dynamicDate.toUTCString(),
-        temperatura: temperatura        
+        temperatura: temperatura,
+        cidade: local        
     });
 }
 
